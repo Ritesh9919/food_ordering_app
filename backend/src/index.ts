3009f34422551d5282419d56119b9f889313def6
Ritesh Maurya <riteshkumar411552@gmail.com>
2025-01-17 14:46:57 +0530

    connected database to backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,12 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
+import mongoose from "mongoose";
+
+// connect to database
+mongoose.connect(process.env.MONGO_URI as string).then(() => {
+  console.log("Database connected!");
+});
 
 const app = express();
 app.use(express.json());
